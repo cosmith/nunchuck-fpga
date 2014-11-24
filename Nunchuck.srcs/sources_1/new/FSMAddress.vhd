c69968ade1a94ea9657099629d44522ec714ab1d
Corentin Smith <corentin.smith@student.ecp.fr>
2014-11-24 10:10:23 +0100

    Add FSM for the address

--- Nunchuck.srcs/sources_1/new/FSMAddress.vhd
+++ Nunchuck.srcs/sources_1/new/FSMAddress.vhd
@@ -4,15 +4,67 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity FSMAddress is
-    Port ( SCL : in STD_LOGIC;
-           SDA : in STD_LOGIC;
-           GoAddress : in STD_LOGIC;
-           DoneAddress : out STD_LOGIC);
+    Port (SCL : in STD_LOGIC;
+          SDAIn : in STD_LOGIC;
+          Address : in STD_LOGIC_VECTOR (1 to 7);
+          GoAddress : in STD_LOGIC;
+          SDAOut : out STD_LOGIC;
+          DoneAddress : out STD_LOGIC);
 end FSMAddress;
 
 architecture Behavioral of FSMAddress is
 
+type STATE_TYPE is (Waiting, Address1, Address2, Address3, Address4, Address5, Address6, Address7); 
+signal state : STATE_TYPE;
+
 begin
 
+FSMAddressTransitions : process(SCL, SDAIn, Address, GoAddress, SDAOut, DoneAddress)
+begin
+    if SCL'event then
+        case state is
+            when Waiting =>
+                if SCL = '1' and GoAddress = '1' then
+                    state <= Address1;
+                end if;
+            when Address1 =>
+                if SCL = '1' then
+                    SDAOut <= Address(1);
+                    state <= Address2;
+                end if;
+            when Address2 =>
+                if SCL = '1' then
+                    SDAOut <= Address(2);
+                    state <= Address3;
+                end if;
+            when Address3 =>
+                if SCL = '1' then
+                    SDAOut <= Address(3);
+                    state <= Address4;
+                end if;
+            when Address4 =>
+                if SCL = '1' then
+                    SDAOut <= Address(4);
+                    state <= Address5;
+                end if;
+            when Address5 =>
+                if SCL = '1' then
+                    SDAOut <= Address(5);
+                    state <= Address6;
+                end if;
+            when Address6 =>
+                if SCL = '1' then
+                    SDAOut <= Address(6);
+                    state <= Address7;
+                end if;
+            when Address7 =>
+                if SCL = '1' then
+                    SDAOut <= Address(7);
+                    state <= Waiting;
+                    DoneAddress <= '1';
+                end if;
+        end case;
+    end if;
+end process;
 
 end Behavioral;
